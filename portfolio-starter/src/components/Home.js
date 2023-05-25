74e81b7f38fd986aba656547e98516be43cf1d1a
Akshay Mistry <akshaymistry@akshaysmbp16495.lan>
2023-05-24 23:55:32 -0400

    6

--- portfolio-starter/src/components/Home.js
+++ portfolio-starter/src/components/Home.js
@@ -137,7 +137,7 @@ const Home = () => {
              initial="hidden"
              whileInView={'show'}
              viewport={{once:false, amount: 0.7}}
-             className='mb-8 max-w-lg mx-auto lg:mx-0 text-[14px] lg:text-[18px]'>
+             className='mb-8 max-w-lg mx-auto lg:mx-0 text-[16px] lg:text-[18px]'>
              Hey, I'm Akshay! I'm an undergraduate student at Georgia Tech studying CS and math, seeking new opportunities to learn and expand my abilities as a developer. I'm interested in full-stack development, machine learning, and data science, and I love to innovate and solve problems. Browse through my site to view my portfolio of work, including my research and some of my favorite personal projects!
            </motion.p>
           
